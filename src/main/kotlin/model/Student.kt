cfd57b07609430935789bd8fecccce35bca17e94
Shodiqq <sodiqvegas@gmail.com>
2021-08-25 22:27:52 +0700

    finishing app

---
+++ src/main/kotlin/model/Student.kt
@@ -0,0 +1,11 @@
+package model
+
+import kotlin.reflect.jvm.internal.impl.resolve.jvm.JvmClassName
+
+data class Student(
+    val name : String,
+    val nim : String,
+    val major : String,
+    val className: String,
+    val university : String
+)
